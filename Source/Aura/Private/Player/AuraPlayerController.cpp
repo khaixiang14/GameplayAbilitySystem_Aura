fe3ed6fe8f5912538f4a097cc5f437a2d5317e08
Khai Xiang <yusuke-ueki@hotmail.com>
2024-11-11 20:01:13 +0800

    Post Process Highlighting

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -8,10 +8,83 @@
 
 #include <ThirdParty/ShaderConductor/ShaderConductor/External/DirectXShaderCompiler/include/dxc/DXIL/DxilConstants.h>
 
+#include "Interaction/EnemyInterface.h"
+
 AAuraPlayerController::AAuraPlayerController()
 {
 	bReplicates = true;
 }
+
+void AAuraPlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
+
+	CursorTrace();
+	
+}
+
+void AAuraPlayerController::CursorTrace()
+{
+	FHitResult CursorHit;
+	GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
+	if (!CursorHit.bBlockingHit) return;
+
+	LastActor = ThisActor;
+	ThisActor = CursorHit.GetActor();
+
+	/**
+	 * Line Trace from cursor. There are several scenarios:
+	 * A. LastActor is null && ThisActor is null.
+	 *		- Do nothing.
+	 * B. LastActor is null && ThisActor is valid.
+	 *		- Hightlight ThisActor.
+	 * C. LastActor is valid && ThisActor is null.
+	 *		- unHightlight LastActor.
+	 * D. Both actors are valid but LastActor != ThisActor
+	 *		- unHighlight LastActor and Highlight ThisActor
+	 * E. Both actors are valid & same actor
+	 *		- Do nothing. 
+	 * **/
+
+	if (LastActor == nullptr)
+	{
+		if (ThisActor != nullptr)
+		{
+			//Case B: highlight thisactor
+			ThisActor->HighlightActor();
+		}
+		else
+		{
+			//Case A: do nothing
+		}
+	}
+	else // LastActor is valid
+	{
+		if (ThisActor == nullptr)
+		{
+			// Case C: unhighlight LastActor
+			LastActor->UnHighlightActor();
+		}
+		else // both actors are valid
+		{
+			if (LastActor != ThisActor)
+			{
+				//Case D: unHighlight LastActor and Highlight ThisActor
+				LastActor->UnHighlightActor();
+				ThisActor->HighlightActor();
+			}
+			else
+			{
+				// Case E
+				//do nothing
+			}
+				
+		}
+	}
+}
+
+
+
 
 void AAuraPlayerController::BeginPlay()
 {
@@ -55,3 +128,4 @@ void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
 		ControlledPawn->AddMovementInput(RightDirection,InputAxisVector.X);
 	}
 }
+
