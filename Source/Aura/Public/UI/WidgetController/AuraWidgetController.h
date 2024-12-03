2067605709abd0cb35e0833c0308ac7f8a2e4bc0
Khai Xiang <yusuke-ueki@hotmail.com>
2024-12-03 19:52:54 +0800

    36 Callbacks for Mana Changes

--- Source/Aura/Public/UI/WidgetController/AuraWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/AuraWidgetController.h
@@ -41,6 +41,10 @@ class AURA_API UAuraWidgetController : public UObject
 public:
 	UFUNCTION(BlueprintCallable)
 	void SetWidgetControllerParams(const FWidgetControllerParams& WCParams);
+	
+	virtual void BroadcastInitialValues();
+
+	virtual void BindCallBacksToDependencies();
 
 protected:
 	UPROPERTY(BlueprintReadOnly,Category ="WidgetController")
